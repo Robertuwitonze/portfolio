0528826166c814023762c5f5fc5175b3b50144fd
rootkitine <robertu20002000@gmail.com>
2023-01-05 20:03:45 +0200

    todo list

--- todo-list/list.js
+++ todo-list/list.js
@@ -19,16 +19,9 @@ input.addEventListener('keyup', () => {
 addbtn.addEventListener('click', () => {
     if(input.value.trim() != 0)
     {
-        let newItem = document.createElement('div');
-        newItem.classList.add('items');
-        newItem.innerHTML='<p>'+ input.value +'</p>'+
-        '<div class="item-btn">'+
-            '<i class="fa-solid fa-check"></i>'+
-            '<i class="fa-sharp fa-solid fa-xmark"></i>'+
-            
-        '</div>';
-
-        tasks.appendChild(newItem);
+        
+        toDo();
+        // tasks.appendChild(newItem);
         input.value = '';
         
     }
@@ -43,6 +36,10 @@ tasks.addEventListener('click', (e) => {
     if(e.target.classList.contains('fa-xmark'))
     {
         e.target.parentElement.parentElement.remove(); 
+        let val = e.target.parentElement.parentElement.getElementsByTagName('input')[0].value;
+       console.log(val);
+
+    deleteTodos(val);
     }
 })
 
@@ -52,3 +49,112 @@ tasks.addEventListener('click', (e) => {
         e.target.parentElement.parentElement.classList.toggle('completed'); 
     }
 })
+
+
+// ==================== blog ============================
+function toDo() {
+   
+ 
+
+
+
+  let title =  document.querySelector('.lists').value;
+ 
+
+
+
+
+ 
+      let todo = {
+        
+        title: title,
+        
+  };
+   console.log(todo);
+
+     if(localStorage.getItem('todolist') === null)
+    {
+      let todolist = [];
+      todolist.push(todo);
+      localStorage.setItem('todolist', JSON.stringify(todolist));
+    }
+  else{
+    let todolist = JSON.parse(localStorage.getItem('todolist'));
+    todolist.push(todo);
+    localStorage.setItem('todolist', JSON.stringify(todolist));
+  }
+
+  getTodos();
+
+
+
+
+
+}
+
+//  ========================== get blogs ---=======================
+  function getTodos()
+{
+   let todolists = JSON.parse(localStorage.getItem('todolist'));
+  console.log(todolists);
+  let blogDiv = document.getElementById('todos');
+  blogDiv.innerHTML="";
+
+  for(var a=0; a< todolists.length; a++)
+
+  {
+  let title = todolists[a].title;
+
+
+// blogDiv.innerHTML += 
+// '<p>'+ todolists[a].title +'</p> <div class="item-btn"><i class="fa-solid fa-check"></i><i class="fa-sharp fa-solid fa-xmark"></i></div>';
+    
+let newItem = document.createElement('div');
+newItem.classList.add('items');
+newItem.innerHTML='<p>'+ todolists[a].title +'</p>'+
+'<input type="hidden" value="'+todolists[a].title +'">'+
+'<div class="item-btn">'+
+    '<i class="fa-solid fa-check"></i>'+
+    '<i class="fa-sharp fa-solid fa-xmark"></i>'+
+    
+'</div>';
+
+tasks.appendChild(newItem);
+input.value = '';
+  }
+}
+
+function deleteTodos( del)
+{
+   let todolists = JSON.parse(localStorage.getItem('todolist'));
+  console.log(todolists);
+  let blogDiv = document.getElementById('todos');
+  blogDiv.innerHTML="";
+
+  for(var a=0; a< todolists.length; a++)
+
+  {
+    if( todolists[a].title == del){
+      todolists.splice(a,1);
+      localStorage.setItem('todolist', JSON.stringify(todolists));
+      getTodos();
+    }
+//   let title = todolists[a].title;
+
+
+// // blogDiv.innerHTML += 
+// // '<p>'+ todolists[a].title +'</p> <div class="item-btn"><i class="fa-solid fa-check"></i><i class="fa-sharp fa-solid fa-xmark"></i></div>';
+    
+// let newItem = document.createElement('div');
+// newItem.classList.add('items');
+// newItem.innerHTML='<p>'+ todolists[a].title +'</p>'+
+// '<div class="item-btn">'+
+//     '<i class="fa-solid fa-check"></i>'+
+//     '<i class="fa-sharp fa-solid fa-xmark"></i>'+
+    
+// '</div>';
+
+// tasks.appendChild(newItem);
+// input.value = '';
+  }
+}
