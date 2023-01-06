ea2d91aa66a432b3b322d1d50bd28656c1e365c7
Robertuwitonze <robertu20002000@gmail.com>
2023-01-06 08:32:55 +0200

    mods

--- js/messsage_validation.js
+++ js/messsage_validation.js
@@ -1,47 +1,53 @@
-function validateForm() {
-   
-    let ntex;
-    let etex;
-    let ptex;
-    let ltex;
-    let mtex;
+let btn = document.getElementById('sendMessage');
+btn.addEventListener('click', function (e)
+{
+    e.preventDefault();
+     
+      let ntex;
+      let etex;
+      let ptex;
+      let ltex;
+      let mtex;
+  
+    let name = document.forms["messageForm"]["name"].value;
+    let email = document.forms["messageForm"]["email"].value;
+    let phone = document.forms["messageForm"]["phone"].value;
+  
+    let location = document.forms["messageForm"]["location"].value;
+    let message = document.forms["messageForm"]["message"].value;
+  
+  
+  
+    if (name == "") {
+      ntex = "Name must be filled out";
+        document.getElementById('ndemo').innerHTML = ntex;
+  
+    }
+    if (email == "") {
+      etex = "email must be filled out";
+        document.getElementById('edemo').innerHTML = etex;
+  
+    }
+     if (phone == "") {
+      ptex = "phone must be filled out";
+        document.getElementById('pdemo').innerHTML = ptex;
+  
+    }
+    if (location == "") {
+      ltex = "location must be filled out";
+        document.getElementById('ldemo').innerHTML = ltex;
+  
+    }
+    if (message == "") {
+      mtex = "message must be filled out";
+        document.getElementById('mdemo').innerHTML = mtex;
+  
+    }
+  
+  
+});
 
-  let name = document.forms["messageForm"]["name"].value;
-  let email = document.forms["messageForm"]["email"].value;
-  let phone = document.forms["messageForm"]["phone"].value;
 
-  let location = document.forms["messageForm"]["location"].value;
-  let message = document.forms["messageForm"]["message"].value;
-
-
-
-  if (name == "") {
-    ntex = "Name must be filled out";
-      document.getElementById('ndemo').innerHTML = ntex;
-
-  }
-  if (email == "") {
-    etex = "email must be filled out";
-      document.getElementById('edemo').innerHTML = etex;
-
-  }
-   if (phone == "") {
-    ptex = "phone must be filled out";
-      document.getElementById('pdemo').innerHTML = ptex;
-
-  }
-  if (location == "") {
-    ltex = "location must be filled out";
-      document.getElementById('ldemo').innerHTML = ltex;
-
-  }
-  if (message == "") {
-    mtex = "message must be filled out";
-      document.getElementById('mdemo').innerHTML = mtex;
-
-  }
-
-}
 
 
 // ==================== blog ============================
